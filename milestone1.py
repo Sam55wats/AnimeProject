4b0559e159bf805619c703e19624e9a49f2a42e3
Samuel  Kim <samuelkim@samuels-mbp-6.mynetworksettings.com>
2024-06-21 16:27:58 -0400

    figured out how to web scrape the short summary

--- milestone1.py
+++ milestone1.py
@@ -3,10 +3,18 @@ import requests
 
 page = requests.get('https://onepiece.fandom.com/wiki/Chapter_1').text
 soup = BeautifulSoup(page, 'lxml')
-title = soup.find('aside', class_ = 'portable-infobox pi-background pi-border-color pi-theme-Chapter pi-layout-default')
+#title = soup.find('aside', class_ = 'portable-infobox pi-background pi-border-color pi-theme-Chapter pi-layout-default')
+
 #finding title of chapter 1 webpage
 title_name = soup.find('h2', class_ = 'pi-item pi-item-spacing pi-title pi-secondary-background').text
-print(title_name)
+##print(title_name)
+
 #finding chapter number
 chapter_num = soup.find('span', 'mw-page-title-main').text
+
+#finding short summary
+long_summary = soup.find('span', id = 'Short_Summary').find_next('p').text
+print(long_summary)
+
+
 
