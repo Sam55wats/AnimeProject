f465311c760f70362a583aaa33ace64743ab0fbf
Samuel  Kim <samuelkim@samuels-mbp-6.mynetworksettings.com>
2024-07-04 13:43:35 -0400

    implemented constrcutor and get_page_content, get_chapter_title, get_chapter_num, get_short_summary methods

--- milestone1.py
+++ milestone1.py
@@ -5,28 +5,27 @@ class AnimeScraper:
     BASE_URL = 'https://onepiece.fandom.com/wiki/Chapter_'
     CHAPTER_COUNT = 200
 
-    def get_chapter_urls(base_url, count):
-        return [f'{base_url}{i}' for i in range(1, count + 1)]
+    def __init__(self, base_url, chapter_count):
+        self.base_url = base_url
+        self.chapter_count = chapter_count
+        self.chapter_urls = self.get_chapter_urls()
+    
+    def get_chapter_urls(self):
+        return [f'{self.base_url}{i}' for i in range(1, self.count + 1)]
 
-    """for url in chapter_urls:
+    def get_page_content(self, url):
         page = requests.get(url).text
-        soup = BeautifulSoup(page, 'lxml')"""
+        return BeautifulSoup(page, 'lxml')
 
-    #print(page)
-    #title = soup.find('aside', class_ = 'portable-infobox pi-background pi-border-color pi-theme-Chapter pi-layout-default')
+    def get_chapter_title(self, soup):
+        return soup.find('h2', class_ = 'pi-item pi-item-spacing pi-title pi-secondary-background').text
 
-    #finding title of chapter 1 webpage
-    #title_name = soup.find('h2', class_ = 'pi-item pi-item-spacing pi-title pi-secondary-background').text
-    ##print(title_name)
+    def get_chapter_num(self, soup):
+        return soup.find('span', 'mw-page-title-main').text
 
-    #finding chapter number
-    #chapter_num = soup.find('span', 'mw-page-title-main').text
+    def get_short_summary(self, soup):
+        return soup.find('span', id = 'Short_Summary').find_next('p').text
 
-    #finding short summary
-    #short_summary = soup.find('span', id = 'Short_Summary').find_next('p').text
-    #print(short_summary)
-
-    #finding long summary
     '''long_heading = soup.find('span', id = 'Long_Summary')
     contents_after = long_heading.parent.find_next_siblings('p')
     long_summary = ""
