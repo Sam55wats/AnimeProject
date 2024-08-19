5210446179d5c03c6ee6887dcef6db9ae304896b
Samuel  Kim <samuelkim@samuels-mbp-6.mynetworksettings.com>
2024-08-18 20:32:43 -0400

    implemented load_summaries and build_model method

--- animanga_analysis.py
+++ animanga_analysis.py
@@ -1,14 +1,20 @@
 import re
+from sklearn.feature_extraction.text import TfidfVectorizer, CountVectorizer
+from sklearn.metrics.pairwise import cosine_similarity
+from sklearn.pipeline import Pipeline, FeatureUnion
 from nltk.stem import WordNetLemmatizer
 from nltk.stem.porter import PorterStemmer
 from nltk.tokenize import word_tokenize
 from nltk.corpus import stopwords
 import constants as const
+import glob
+import json
+import os
 
 class Analyzer:
-    def preprocessor(summary):
+    def preprocessor(self, summary):
         summary = summary.lower()
-        #removes all characters that are not letters and num, sub replaces occurences of pattern in string with replacement
+        #removes all characters that are not letters and num, sub replaces occurences of pattern with replacement
         summary = re.sub("[^a-zA-Z0-9]", " ", summary) # ^ is negation
         tokens = word_tokenize(summary) #splits text into individual words
 
@@ -25,4 +31,37 @@ class Analyzer:
             clean_tokens.append(clean_tok)
 
         return clean_tokens
+    
+    def load_summaries(self, directory):
+        summaries = []
+        for filename in glob.glob(os.path.join(directory, '*.json')):
+            with open(filename, 'r') as f:
+                data = json.load(f)
+                summaries.append(data['short_summary'])
+        return summaries
 
+    def preprocess_summaries(self, summaries):
+        preprocessed = []
+        for summary in summaries:
+            preprocessed
+
+    def build_model(self):
+        """Build the model
+
+        Returns
+        -------
+        sklearn.pipeline.Pipeline
+            The model
+        """
+        pipeline = Pipeline([
+            (const.FEATURES, FeatureUnion([
+
+                (const.TEXT_PIPELINE, Pipeline([
+                    (const.VECT, CountVectorizer(tokenizer=self.preprocessor)),
+                    (const.TFIDF, TfidfTransformer())
+                ]))
+            ]))
+        ])
+        
+        return pipeline
+ 
